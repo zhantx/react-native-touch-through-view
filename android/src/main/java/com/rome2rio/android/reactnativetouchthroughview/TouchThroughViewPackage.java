3680030db7e751c15f6158aaabaea7b415c1f37c
Ilhamy Putra <miputra@student.unimelb.edu.au>
2018-01-17 16:47:06 +1100

    Update TouchThroughViewPackage.java

--- android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
+++ android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
@@ -25,11 +25,6 @@ public class TouchThroughViewPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         TouchThroughViewManager touchThroughViewManager = new TouchThroughViewManager();
         TouchThroughWrapperManager touchThroughWrapperManager = new TouchThroughWrapperManager();
